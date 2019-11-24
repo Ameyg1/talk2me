24c84ecc8c86f6da2615537a883ccaf991958213
Ameyg1 <ameyg1@gmail.com>
2019-11-24 15:04:45 +1300

    build

---
+++ build/precache-manifest.268a9583371a71b5370dbf230ad91e0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef3b091aa00e18282f2505679fc77acb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da40760e3932784f1c93",
+    "url": "/static/css/main.95b9bf3d.chunk.css"
+  },
+  {
+    "revision": "0245e54741ebe77bd7c1",
+    "url": "/static/js/2.3e8bd94d.chunk.js"
+  },
+  {
+    "revision": "da40760e3932784f1c93",
+    "url": "/static/js/main.85299575.chunk.js"
+  },
+  {
+    "revision": "381596870c86d5af86e1",
+    "url": "/static/js/runtime-main.a2310626.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
