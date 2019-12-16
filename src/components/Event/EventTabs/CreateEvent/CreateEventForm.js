8999081d56552dea63e40767b00b505263077daf
abir4u <abirpal90@gmail.com>
2019-12-16 14:54:42 +1300

    working on create event api handling

--- src/components/Event/EventTabs/CreateEvent/CreateEventForm.js
+++ src/components/Event/EventTabs/CreateEvent/CreateEventForm.js
@@ -10,8 +10,8 @@ export default class CreateEventForm extends React.Component {
     this.state = {
       EVENT_ID: "",
       EVENT_NAME: "",
-      ORGANISER: "",
       ORG_EMAIL: "",
+      ORGANISER: "",
       VENUE: "",
       PURPOSE: ""
     };
@@ -19,42 +19,50 @@ export default class CreateEventForm extends React.Component {
 
   /** Checks if mandatory fields are filled */
   isRequiredFieldsFilled() {
-    return this.state.NAME && this.state.TITLE && this.state.EMAIL;
+    return (
+      this.state.EVENT_NAME && this.state.ORG_EMAIL && this.state.ORGANISER
+    );
   }
 
-  createEvennt = e => {
+  createEvent = e => {
+    console.log("In");
+    console.log(
+      this.state.EVENT_NAME && this.state.ORG_EMAIL && this.state.ORGANISER
+        ? true
+        : false
+    );
     if (this.isRequiredFieldsFilled()) {
+      console.log("In again");
       e.preventDefault();
       const event = {
-        EVENT_ID: this.state.EVENT_ID,
         EVENT_NAME: this.state.EVENT_NAME,
-        ORGANISER: this.state.ORGANISER,
         ORG_EMAIL: this.state.ORG_EMAIL,
+        ORGANISER: this.state.ORGANISER,
         VENUE: this.state.VENUE,
         PURPOSE: this.state.PURPOSE
       };
-      axios
-        .post(
-          `https://kunektapi.azurewebsites.net/api/event/` +
-            this.state.EVENT_ID,
-          event
-        )
-        .then(
-          res => {
-            this.resetState();
-            this.displayMessageFor("success");
-          },
-          err => {
-            this.displayMessageFor("request failure");
-          }
-        );
-      this.displayMessageFor("waiting");
+      axios.post(`https://kunektapi.azurewebsites.net/api/event`, event).then(
+        async res => {
+          await console.log(res.data.response.insertId);
+          // await console.log(res.data.response[0].insertId);
+          // this.displayMessageFor("success");
+        },
+        err => {
+          // this.displayMessageFor("request failure");
+        }
+      );
+      // this.displayMessageFor("waiting");
     } else {
-      this.displayMessageFor("failure");
+      // this.displayMessageFor("failure");
     }
   };
 
-  handleText = e => {};
+  handleText = (e, name) => {
+    this.setState({ [e.target.name]: e.target.value }, () => {
+      // this.checkData(this.rexExpMap[name], this.state[name], name);
+    });
+  };
+
   render() {
     return (
       <div className="container">
@@ -65,58 +73,54 @@ export default class CreateEventForm extends React.Component {
           <KTextField
             fieldTitle="Event Name *"
             type="text"
-            value={this.state.NAME}
-            fieldName="NAME"
-            onChange={e => this.handleText(e)}
+            value={this.state.EVENT_NAME}
+            fieldName="EVENT_NAME"
+            onChange={e => this.handleText(e, "EVENT_NAME")}
           />
           <KTextField
             fieldTitle="Name of Organiser *"
             type="text"
-            value={this.state.TITLE}
-            fieldName="TITLE"
-            onChange={e => this.handleText(e)}
+            value={this.state.ORGANISER}
+            fieldName="ORGANISER"
+            onChange={e => this.handleText(e, "ORGANISER")}
           />
           <KTextField
             fieldTitle="Email *"
             type="text"
-            value={this.state.EMAIL}
-            fieldName="EMAIL"
-            onChange={e => this.handleText(e)}
+            value={this.state.ORG_EMAIL}
+            fieldName="ORG_EMAIL"
+            onChange={e => this.handleText(e, "ORG_EMAIL")}
           />
           <KTextField
             fieldTitle="Venue"
             type="text"
-            value={this.state.COMPANY}
-            fieldName="COMPANY"
-            onChange={e => this.handleText(e)}
+            value={this.state.VENUE}
+            fieldName="VENUE"
+            onChange={e => this.handleText(e, "VENUE")}
           />
           <KTextField
             fieldTitle="This meetup is for..."
             type="textarea"
-            value={this.state.BIO}
-            fieldName="BIO"
-            onChange={e => this.handleText(e)}
+            value={this.state.PURPOSE}
+            fieldName="PURPOSE"
+            onChange={e => this.handleText(e, "PURPOSE")}
             helpMessage="Type in upto 100 characters."
           />
           <div className="sb-text">
             By clicking Submit, I agree that I have read and accepted the&nbsp;
             <a href="TermsandConditions">Terms and Conditions.</a>
           </div>
-          <button
-            className="sb-btn"
-            type="submit"
-            onClick={this.props.onCompletion}
-          >
+          <button className="sb-btn" type="submit" onClick={this.createEvent}>
             SUBMIT
           </button>
         </div>
-        {this.state.modalisOpen ? (
+        {/* {this.state.modalisOpen ? (
           <Modal
             text="Your Data"
             {...this.state}
             closeModal={this.toggleModal}
           />
-        ) : null}
+        ) : null} */}
       </div>
     );
   }
