1fb9aee708fd8574bb6a3f7cdb6c7d6f1c1bc623
abir4u <abirpal90@gmail.com>
2019-12-16 17:25:43 +1300

    fixing pop up UI issues for diferent browsers on real device

--- src/Reusables/EnvironmentVariables.js
+++ src/Reusables/EnvironmentVariables.js
@@ -1,6 +1,6 @@
 export const env_variable = {
-  PROD_URL: "https://master.d2ymg4og9qeqjh.amplifyapp.com/",
-  LOCAL_URL: "http://localhost:3000/"
+  PROD_URL: "https://master.d2ymg4og9qeqjh.amplifyapp.com",
+  LOCAL_URL: "http://localhost:3000"
 };
 
 export default env_variable;
